8fecae76dcd908381c30b8ccead0a84a576602fd
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-31 19:48:38 +0000

    Create ubuntu-unity-install.sh

---
+++ ubuntu-unity-install.sh
@@ -0,0 +1,18 @@
+sudo apt-add repository main -yy
+
+sudo apt-add repository restricted -yy
+
+sudo apt-add repository universe -yy
+
+sudo apt-add repository multiverse -yy
+
+sudo apt update -yy
+
+sudo apt upgrade -yy
+
+sudo apt install lightdm-gtk-greeter lightdm-gtk-greeter-settings -yy
+
+sudo apt install ubuntu-unity-desktop -yy
+
+sudo systemctl enable lightdm
+
